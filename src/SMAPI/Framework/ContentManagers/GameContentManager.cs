6068d6ce2ab890a555321cbc17ce396c7b2ccc91
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2024-03-09 17:20:28 -0500

    clarify error message when SMAPI blocks a map load

--- src/SMAPI/Framework/ContentManagers/GameContentManager.cs
+++ src/SMAPI/Framework/ContentManagers/GameContentManager.cs
@@ -345,7 +345,7 @@ namespace StardewModdingAPI.Framework.ContentManagers
                     // handle mismatch
                     if (loadedMap.TileSheets.Count <= vanillaSheet.Index || loadedMap.TileSheets[vanillaSheet.Index].Id != vanillaSheet.Id)
                     {
-                        mod.LogAsMod($"SMAPI found an issue with a '{info.Name}' map load: {this.GetOnBehalfOfLabel(loader.OnBehalfOf, parenthetical: false) ?? "mod"} reordered the original tilesheets, which often causes crashes.\nTechnical details for mod author: Expected order: {string.Join(", ", vanillaTilesheetRefs.Select(p => p.Id))}. See https://stardewvalleywiki.com/Modding:Maps#Tilesheet_order for help.", LogLevel.Error);
+                        mod.LogAsMod($"SMAPI blocked a '{info.Name}' map load: {this.GetOnBehalfOfLabel(loader.OnBehalfOf, parenthetical: false) ?? "mod"} reordered the original tilesheets, which often causes crashes.\nTechnical details for mod author: Expected order: {string.Join(", ", vanillaTilesheetRefs.Select(p => p.Id))}. See https://stardewvalleywiki.com/Modding:Maps#Tilesheet_order for help.", LogLevel.Error);
                         return false;
                     }
                 }
